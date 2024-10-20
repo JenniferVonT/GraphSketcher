e8bc81d8f4eb3da4f712e4e1680d2a8ac03a6c36
Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
2024-10-20 03:10:48 +0200

    Test changing some functions in the project to see if the launched version updates aswell

--- src/view/view.js
+++ src/view/view.js
@@ -16,9 +16,28 @@ export default class View {
   }
 
   startPage () {
-    const doc = document.querySelector('body')
+    const doc = document.querySelector('main')
 
     doc.append(document.createElement('p').textContent = 'test hejsan!')
+
+    const navStart = document.querySelector('#nav_start')
+    const navEdit = document.querySelector('#nav_edit')
+    const navSaved = document.querySelector('#nav_saved')
+
+    navStart.addEventListener('click', () => {
+      const p = document.createElement('p')
+      doc.append(p.textContent = 'START PAGE! \n')
+    })
+
+    navEdit.addEventListener('click', () => {
+      const p = document.createElement('p')
+      doc.append(p.textContent = 'EDIT PAGE! \n')
+    })
+
+    navSaved.addEventListener('click', () => {
+      const p = document.createElement('p')
+      doc.append(p.textContent = 'SAVED PAGE! \n')
+    })
 
     this.#controller.test()
   }
